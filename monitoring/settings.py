04b3b3b459f04b2be9fd95c065536326f89e5514
Yei Hong <yh.zhangc@uniandes.edu.co>
2022-08-26 21:36:40 -0500

    Taller 3.1 DONE

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 DATABASES = {
      "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "<nombrebd>",
-        "USER": "<usuario>",
-        "PASSWORD": "<clave>",
+        "NAME": 'azeroth',
+        "USER": 'yh',
+        "PASSWORD": 'YH',
         "HOST": "localhost",
         "PORT": "",
     }
